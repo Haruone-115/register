0aa5fa12f6f34f11de29c306e99add65b5e2340f
homu <haruichi2002@gmail.com>
2022-11-08 16:51:22 +0900

    useState genius

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,103 +2,71 @@ import Head from 'next/head'
 import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 import { Component, MouseEvent } from 'react';
-import React from 'react';
+import React, { useState } from 'react';
 import { throws } from 'assert';
 
 const Product = "商品";
-const Label = "商品A";
-var table_elements;
+const Label1 = "商品A";
+const Label2 = "商品B";
+const Label3 = "商品C";
 
-/*
-function App() {
+export default function App() {
 
-  const handleClick = (event: any) => {    {/*ボタンが押されたときの処理 
-    console.log(event.currentTarget);
-    console.log(event);
-    table_elements = document.getElementById('product_name');
+  const initialState = [];
+  const [productname, setProductname] = useState(initialState);
+  const [flag, setFlag] = useState(0);
 
-    table_elements!.textContent= Label;
-
+  const productClick_1 = () => {
+    setFlag(flag + 1);
+    setProductname([...productname, Label1]);
   }
 
-
-  return (
-    <>
-      <div className="App">
-        <button className={styles.btn__orange} onClick={handleClick}>{Label}</button>  {/*ボタンの記述、CSSの参照 
-      </div>
-      <table border={1}>
-        <tr>
-          <th className={styles.table_th}>{Product}</th>
-        </tr>
-        <tr>
-          <td className={styles.table_td} id="product_name"></td>
-        </tr>
-      </table>
-    </>
-  );
-}
-
-export default App;
-
-*/
-class Toggle extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isToggleOn: true,
-      productname: '',
-      flag: 0,
-    };
-    this.delateClick = this.delateClick.bind(this);
+  const productClick_2 = () => {
+    setFlag(flag + 1);
+    setProductname([...productname, Label2]);
   }
 
-  handleClick = () => {
-    this.setState(state => ({
-      isToggleOn: !state.isToggleOn,
-      productname: Label,
-      flag: this.state.flag + 1,
-    }));
+  const productClick_3 = () => {
+    setFlag(flag + 1);
+    setProductname([...productname, Label3]);
   }
 
-  delateClick = () => {
-    this.setState(state => ({
-      isToggleOn: !state.isToggleOn,
-      flag: 0,
-      productname: "",
-    }));
+  const delateClick = () => {
+    setFlag(0);
+    setProductname ([]);
     alert("delate");
   }
 
-  render() {
     //const productname = this.state;
     return (
-      <>
-
-        <button className={styles.btn__orange} onClick={this.handleClick}>
-          {Label}
-        </button>
-        <button className={styles.btn__orange_1} onClick={this.delateClick}>
-          削除
-        </button>
-        <table border={2}>
-        <tr>
-          <th className={styles.table_th}>{Product}</th>
-        </tr>
-        <tr>
-          <td className={styles.table_td}>{this.state.flag >= 1 ? this.state.productname : ""}</td>
-        </tr>
-        <tr>  
-          <td className={styles.table_td}>{this.state.flag >= 2 ? this.state.productname : ""}</td>        
-        </tr>
-        <tr>
-          <td className={styles.table_td}>{this.state.flag >= 3 ? this.state.productname : ""}</td>
-        </tr>
-        </table>
-      </>
-      
-    );
-  }
+    <>
+      <button className={styles.btn__orange_1} onClick={productClick_1}>
+        {Label1}
+      </button>
+      <button className={styles.btn__orange_2} onClick={productClick_2}>
+        {Label2}
+      </button>
+      <button className={styles.btn__orange_3} onClick={productClick_3}>
+        {Label3}
+      </button>
+      <button className={styles.btn__delete} onClick={delateClick}>
+        削除
+      </button>
+      <table border={2}>
+      <tr>
+        <th className={styles.table_th}>{Product}</th>
+      </tr>
+      <tr>
+        <td className={styles.table_td}>{flag >= 1 ? productname[0] : ""}</td>
+      </tr>
+      <tr>  
+        <td className={styles.table_td}>{flag >= 2 ? productname[1] : ""}</td>        
+      </tr>
+      <tr>
+        <td className={styles.table_td}>{flag >= 3 ? productname[2] : ""}</td>
+      </tr>
+      </table>
+    </>
+    
+  );
 }
-
-export default Toggle;
