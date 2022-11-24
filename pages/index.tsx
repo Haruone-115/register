f473ebcb51fb62a613208bcb510fee6f254157dd
homu <haruichi2002@gmail.com>
2022-11-24 13:50:56 +0900

    Tab実装

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import styles from '../styles/Home.module.css'
 import { Component, MouseEvent } from 'react';
 import React, { useState, useRef } from 'react';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 import { throws } from 'assert';
 import Table from '../components/table'
 import Modal from '../components/Modal'
@@ -30,22 +31,33 @@ export default function App() {
 
     return (
     <>
-    <div>
-      <Modal Label={Label1} setLabel={setLabel1} show={show1} setShow={setShow1} />
-      <ProductClick Label={Label1} setShow={setShow1} setFlag={setFlag} flag={flag} setProductname={setProductname} productname={productname} styles_name={styles.btn__orange_1} />
-    </div>
-    <div>
-      <Modal Label={Label2} setLabel={setLabel2} show={show2} setShow={setShow2} />
-      <ProductClick Label={Label2} setShow={setShow2} setFlag={setFlag} flag={flag} setProductname={setProductname} productname={productname} styles_name={styles.btn__orange_2} />
-    </div>
-    <div>
-      <Modal Label={Label3} setLabel={setLabel3} show={show3} setShow={setShow3} />
-      <ProductClick Label={Label3} setShow={setShow3} setFlag={setFlag} flag={flag} setProductname={setProductname} productname={productname} styles_name={styles.btn__orange_3} />
-    </div>
-    <button className={styles.btn__delete} onClick={delateClick}>
-      削除
-    </button>
-    <Table Product={Product} flag = {flag} productname = {productname} />
+      <Tabs>
+        <TabList>
+          <Tab>1</Tab>
+          <Tab>2</Tab>
+          <Tab>3</Tab>
+        </TabList>
+      
+
+        <TabPanel>
+          <div>
+            <Modal Label={Label1} setLabel={setLabel1} show={show1} setShow={setShow1} />
+            <ProductClick Label={Label1} setShow={setShow1} setFlag={setFlag} flag={flag} setProductname={setProductname} productname={productname} styles_name={styles.btn__orange_1} />
+          </div>
+          <div>
+            <Modal Label={Label2} setLabel={setLabel2} show={show2} setShow={setShow2} />
+            <ProductClick Label={Label2} setShow={setShow2} setFlag={setFlag} flag={flag} setProductname={setProductname} productname={productname} styles_name={styles.btn__orange_2} />
+          </div>
+          <div>
+            <Modal Label={Label3} setLabel={setLabel3} show={show3} setShow={setShow3} />
+            <ProductClick Label={Label3} setShow={setShow3} setFlag={setFlag} flag={flag} setProductname={setProductname} productname={productname} styles_name={styles.btn__orange_3} />
+          </div>
+          <button className={styles.btn__delete} onClick={delateClick}>
+            削除
+          </button>
+        </TabPanel>
+        <Table Product={Product} flag = {flag} productname = {productname} />
+      </Tabs>
     </>
     
   );
